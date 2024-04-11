1c28ba2be1821a1e9851444b708c85317ba32a13
Soham Jaiswal <sosweetham@gmail.com>
2024-04-11 12:11:47 +0530

    fix: missing image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <div class="bg-[url('/images/bg.jpeg')] h-[75vh] w-screen bg-no-repeat bg-cover -scale-x-100">
 	<div
 		class="bg-[url('/images/hero-rem.png')] h-[75vh] w-full bg-no-repeat bg-right-bottom bg-contain -scale-x-100"
-	/>
+	>
+		⠀
+	</div>
 </div>
 <div id="stripe-bound">
 	<div id="stripe-container">
