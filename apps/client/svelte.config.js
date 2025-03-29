c5e70a4ea9ddbb3fd587919bc061fca227ca70c0
Soham Jaiswal <sosweetham@gmail.com>
2025-03-29 12:28:28 +0530

    fix: reinstance client

--- apps/client/svelte.config.js
+++ apps/client/svelte.config.js
@@ -1,22 +1,21 @@
+import { mdsvex } from "mdsvex";
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import nesting from 'postcss-nesting';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), nesting()],
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: [vitePreprocess(), mdsvex()],
 
-	vitePlugin: {
-		inspector: true
-	},
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+
+    extensions: [".svelte", ".svx"]
 };
+
 export default config;
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,16 +1,6 @@
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		purgeCss({
-      legacy: true,
-			safelist: {
-				// any selectors that begin with "hljs-" will not be purged
-				greedy: [/^hljs-/]
-			}
-		})
-	]
+    plugins: [sveltekit()],
 });
